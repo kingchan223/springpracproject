b7e775f3d0b6b09c9a0cf7fc0281446243a52a75
이찬영 <kokiyo97@naver.com>
2021-02-28 00:25:56 +0900

    환경설정

---
+++ src/main/java/com/example/springpracproject/SpringpracprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpracprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpracprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpracproject/SpringpracprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpracprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
